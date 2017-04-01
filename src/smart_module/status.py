6c531bec95090eae576abf84993f2155df351c05
Pedro Freitas <pedrosffreitas@gmail.com>
2017-04-01 12:30:43 -0300

    Comments, disk usage, TODO list and tabs

--- src/smart_module/status.py
+++ src/smart_module/status.py
@@ -1,12 +1,11 @@
 #!/bin/python
-# or #!/bin/env python3
 
 # HAPI Project
-# Smart Module Status is responsible to fetch information from the system
+# Smart Module Status is responsible for fetching information from the system
 # Author: Pedro Freitas
 # Release: 0.1-alpha
 # Copyright 2017 Maya Culpa, LLC
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -20,48 +19,65 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# We should get:
+# Done
 # 1. Memory:
-# 	1.1. Memory used;
-# 	1.2. Memory available; and
-# 	1.3. Memory Process time.
+#   1.1. Memory used;
+#   1.2. Memory available;
 # 2. Disk:
-# 	2.1. Disk used; and
-# 	2.2. Disk available.
+#   2.1. Disk used;
+#   2.2. Disk available.
 # 3. Networking
-# 	3.1. Network active connections;
-# 	3.2. Network sent packets; and
-# 	3.3. Network received packets.
+#   3.2. Network sent packets; and
+#   3.3. Network received packets.
 # 4. Boot:
-# 	4.1. Boot time.
+#   4.1. Boot time.
 # 5. CPU:
-# 	5.1. CPU Process time
+#   5.1. CPU Process time
+# TODO:
+#   3.1. Network active connections;
+#   1.3. Memory Process time.
 
 import psutil
+"""
+This is a wrapper on psutil to provide system information
+in a simple JSON way. If run stand-alone, print all information.
+"""
 
 class SystemStatus:
-	""" Small class to handle system information """
+    """ Small class to handle system information """
 
-	def __init__(self):
-		self.cpu = { "percentage": 0 }
-		self.boot = { "time": 0 }
-		self.memory = { "used": 0, "free": 0 }
-		self.network = { "packet_sent": 0, "packet_recv": 0 }
+    def __init__(self):
+        self.cpu = { "percentage": 0 }
+        self.boot = { "time": 0 }
+        self.memory = { "used": 0, "free": 0 }
+        self.network = { "packet_sent": 0, "packet_recv": 0 }
+        self.disk = { "total": 0, "used": 0, "free": 0 }
 
-	def __str__(self):
-		json_string = str('"memory": {}, "cpu": {}, "boot": {}, "network": {}')
-		json_info = '{' + json_string.format(self.memory, self.cpu, self.boot, self.network) + '}'
-		return json_info
+    def __str__(self):
+        json_string = str('"memory": {}, "cpu": {}, "boot": {}, "network": {}, "disk": {}')
+        json_info = '{' + json_string.format(self.memory, self.cpu, self.boot, self.network, self.disk) + '}'
+        return json_info
 
-	def update(self):
-		self.cpu["percentage"] = psutil.cpu_percent(interval=0.7)
-		self.boot["time"] = psutil.boot_time()
-		self.memory["used"] = psutil.virtual_memory()[3]
-		self.memory["free"] = psutil.virtual_memory()[4]
-		self.network["packet_sent"] = psutil.net_io_counters()[2]
-		self.network["packet_recv"] = psutil.net_io_counters()[3]
+    def update(self):
+        """ Function to update the entire class information """
+        self.cpu["percentage"] = psutil.cpu_percent(interval=0.7)
+        self.boot["time"] = psutil.boot_time()
+        # Fetch all information about Memory in a temp variable
+        # then assign each value to a specific key (psutil usually returns
+        # a named tuple
+        tempmemoryinfo = psutil.virtual_memory()
+        self.memory["used"] = tempmemoryinfo[3]
+        self.memory["free"] = tempmemoryinfo[4]
+        tempnetworkinfo = psutil.net_io_counters()
+        self.network["packet_sent"] = tempnetworkinfo[2]
+        self.network["packet_recv"] = tempnetworkinfo[3]
+        tempdiskinfo = psutil.disk_usage('/')
+        self.disk["total"] = tempdiskinfo[0]
+        self.disk["used"] = tempdiskinfo[1]
+        self.disk["free"] = tempdiskinfo[2]
 
 if __name__ == "__main__":
-	ss = SystemStatus()
-	ss.update()
-	print(ss)
+    """ If run as main, print in JSON system information """
+    ss = SystemStatus()
+    ss.update()
+    print(ss)
