636d4a0530ae85edeb16c3de91c1cb3d724fa750
Pedro Freitas <pedrosffreitas@gmail.com>
2017-04-02 20:57:09 -0300

    Improving code quality and added time of update

--- src/smart_module/status.py
+++ src/smart_module/status.py
@@ -18,53 +18,40 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Done
-# 1. Memory:
-#   1.1. Memory used;
-#   1.2. Memory available;
-# 2. Disk:
-#   2.1. Disk used;
-#   2.2. Disk available.
-# 3. Networking
-#   3.2. Network sent packets; and
-#   3.3. Network received packets.
-# 4. Boot:
-#   4.1. Boot time.
-# 5. CPU:
-#   5.1. CPU Process time
-# TODO:
-#   3.1. Network active connections;
-#   1.3. Memory Process time.
-
-import psutil
 """
 This is a wrapper on psutil to provide system information
 in a simple JSON way. If run stand-alone, print all information.
 """
 
+# Todo:
+#   3.1. Network active connections;
+#   1.3. Memory Process time.
+
+import time
+import psutil
+
 class SystemStatus:
     """ Small class to handle system information """
 
     def __init__(self, update=False):
-        """ If update == True, create the object and fetch all data """ 
-        self.cpu = { "percentage": 0 }
-        self.boot = { "time": 0 }
-        self.memory = { "used": 0, "free": 0 }
-        self.network = { "packet_sent": 0, "packet_recv": 0 }
-        self.disk = { "total": 0, "used": 0, "free": 0 }
-        if update == True:
+        """ If update == True, create the object and fetch all data """
+        self.cpu = {"percentage": 0}
+        self.bootdate = 0
+        self.memory = {"used": 0, "free": 0}
+        self.network = {"packet_sent": 0, "packet_recv": 0}
+        self.disk = {"total": 0, "used": 0, "free": 0}
+        self.timestamp = 0
+        if update is True:
             self.update()
 
     def __str__(self):
-        json_string = str('"memory": {}, "cpu": {}, "boot": {}, "network": {}, "disk": {}')
-        json_info = '{' + json_string.format(self.memory, self.cpu, self.boot, self.network, self.disk) + '}'
-        return json_info
+        return str([{"time": self.timestamp, "memory": self.memory, "cpu": self.cpu,
+                     "boot": self.boot, "network": self.network, "disk": self.disk}])
 
     def update(self):
         """ Function to update the entire class information """
         self.cpu["percentage"] = psutil.cpu_percent(interval=0.7)
-        self.boot["time"] = psutil.boot_time()
+        self.boot = psutil.boot_time()
         # Fetch all information about Memory in a temp variable
         # then assign each value to a specific key (psutil usually returns
         # a named tuple)
@@ -78,8 +65,8 @@ class SystemStatus:
         self.disk["total"] = tempdiskinfo[0]
         self.disk["used"] = tempdiskinfo[1]
         self.disk["free"] = tempdiskinfo[2]
+        self.timestamp = time.time()
 
 if __name__ == "__main__":
-    """ If run as main, print in JSON system information """
-    ss = SystemStatus(update=True)
-    print(ss)
+    SYSINFO = SystemStatus(update=True)
+    print(SYSINFO)
