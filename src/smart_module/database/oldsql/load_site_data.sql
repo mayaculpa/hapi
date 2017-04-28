bc2f680dd067ad7b2fa4ba513e88cbbcc0c4d295
ozarchie <jmarchbold@gmail.com>
2017-04-28 13:49:06 +1000

    Original test sql files

---
+++ src/smart_module/database/oldsql/load_site_data.sql
@@ -0,0 +1,21 @@
+-- HAPI Data Table Creation Script v1.0
+-- Author: Tyler Reed
+-- Release: June 24th, 2016
+--*********************************************************************
+--Copyright 2016 Maya Culpa, LLC
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--*********************************************************************
+INSERT INTO site(site_id, name, wunder_key, operator, email, phone, location, longitude, latitude)
+VALUES (1, "HPF-0", " ffb22aac10a07be6", "Maya Culpa, LLC", "tyler@hapihq.com", "(614) 316-4312", "Columbus, OH", "", "");
