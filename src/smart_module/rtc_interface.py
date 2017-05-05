21794272354edebc2c0660fa0ffa07b971c4ced2
TylerReedMC <tyler@mayaculpa.com>
2017-05-05 09:21:13 -0400

    Fixed offset bug. Converted hard-coded lengths to constants. Added exception handling, logging and code documentation. Tested and debugged everything.

--- src/smart_module/rtc_interface.py
+++ src/smart_module/rtc_interface.py
@@ -3,8 +3,7 @@
 
 '''
 HAPI Asset Interface - v1.0
-Authors: Tyler Reed
-Release: April 2017, Beta Milestone
+Release: May 2017, Beta Milestone
 Copyright 2016 Maya Culpa, LLC
 
 This program is free software: you can redistribute it and/or modify
@@ -25,127 +24,174 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import SDL_DS3231
 import sys
 import datetime
+import logging
 
-sm_logger = "smart_module"
+SM_LOGGER = "smart_module"
+
+LEN_ID = 16
+LEN_CONTEXT = 16
+LEN_TYPE = 2
 
 class RTCInterface(object):
-    '''Interface for DS3231 Real-time Clock with AT24C32 EEPROM and internal temp sensor
+    '''Interface for DS3231 Real-time Clock with internal temp sensor and AT24C32 EEPROM
     '''
-    def __init__(self, mock):
+
+    def __init__(self, mock=False):
         '''
         Args:
-            mock (bool): Set to True is a hardware RTC is not connected
+            mock (bool): Pass as True if a hardware RTC is not connected
         '''
+
         self.mock = mock
-        if not self.mock:
-            self.ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68, 0x57)
+        self.logger = logging.getLogger(SM_LOGGER)
+
+        try:
+            if not self.mock:
+                self.ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68, 0x57)
+        except Exception, excpt:
+            self.logger.exception("Error initializing RTC. %s", excpt)
 
     def get_datetime(self):
-        '''
+        '''Gets the current date/time from the attached RTC
         Returns:
             datetime: Current date/time from RTC if mock is False. Current Python datetime if mock is True
         '''
-        if self.mock:
-            return datatime.datetime.now()
-        else:
-            return self.ds3231.read_datetime()
+        try:
+            if self.mock:
+                return datatime.datetime.now()
+            else:
+                return self.ds3231.read_datetime()
+        except Exception, excpt:
+            self.logger.exception("Error getting RTC date/time. %s", excpt)
 
     def set_datetime(self):
+        '''Writes the system datetime to the attached RTC (mock is False)
         '''
-        Sets the RTC date/time to Python date/time if mock is False
-        '''
-        if not self.mock:
-            self.ds3231.write_now()
+
+        try:
+            if not self.mock:
+                self.ds3231.write_now()
+        except Exception, excpt:
+            self.logger.exception("Error writing date/time to RTC. %s", excpt)
 
     def get_temp(self):
-        '''
+        '''Gets the internal temperature from the RTC component
         Returns:
             float: Current RTC internal temperature sensor value if mock is False. 20.0 if mock is True
         '''
 
-        if self.mock:
-            return float(random.randrange(8, 34, 1))
-        else:
-            return self.ds3231.getTemp()
+        try:
+            if self.mock:
+                return float(random.randrange(8, 34, 1))
+            else:
+                return self.ds3231.getTemp()
+        except Exception, excpt:
+            self.logger.exception("Error getting the temperature from the RTC. %s", excpt)
 
     def get_type(self):
-        '''
+        '''Gets the modules sensor type from the EEPROM
         Returns:
-            str: 2-byte Type data as String if mock is False. "WT" if mock is True
+            str: 2-byte Type data as String if mock is False. "wt" if mock is True
         '''
-        if self.mock:
-            return "WT"
-        else:
-            byte0 = self.ds3231.read_AT24C32_byte(0)
-            byte1 = self.ds3231.read_AT24C32_byte(1)
-            return str(byte0) + str(byte1)
+
+        try:
+            if self.mock:
+                return "wt"
+            else:
+                byte0 = self.ds3231.read_AT24C32_byte(0)
+                byte1 = self.ds3231.read_AT24C32_byte(1)
+                return str(chr(byte0) + chr(byte1)).strip()
+        except Exception, excpt:
+            self.logger.exception("Error reading type from EEPROM. %s", excpt)
 
     def set_type(self, type_data):
-        '''
+        '''Writes the modules 2-byte sensor type to EEPROM
         Args:
             type_data (str): Sensor Type to write to EEPROM
         '''
-        if not self.mock:
-            self.ds3231.write_AT24C32_byte(0, ord(type_data[0]))
-            self.ds3231.write_AT24C32_byte(1, ord(type_data[1]))
+
+        try:
+            if not self.mock:
+                self.ds3231.write_AT24C32_byte(0, ord(type_data[0]))
+                self.ds3231.write_AT24C32_byte(1, ord(type_data[1]))
+        except Exception, excpt:
+            self.logger.exception("Error writing Sensor Type to EEPROM. %s", excpt)
 
     def get_id(self):
-        '''
+        '''Gets the Smart Module ID from the attached EEPROM
         Returns:
-            str: 16-byte Smart Module ID if mock is False. "HSM-WT123-MOCK" if mock is True
+            str: 16-byte module ID if mock is False. "HSM-WT123-MOCK" if mock is True
         '''
 
-        if self.mock:
-            return "HSM-WT123-MOCK"
-        else:
-            #Concatenate the 16 byte unique sensor address
-            id = ""
-            for x in range(2,18):
-                id = id + str(self.ds3231.read_AT24C32_byte(x))
-            return id
+        try:
+            if self.mock:
+                return "HSM-WT123-MOCK"
+            else:
+                #Concatenate the 16 byte module ID
+                id_data = ""
+                for x in range(LEN_TYPE, LEN_TYPE + LEN_ID):
+                    id_data = id_data + chr(self.ds3231.read_AT24C32_byte(x))
+                return str(id_data).strip()
+        except Exception, excpt:
+            self.logger.exception("Error reading Module ID from EEPROM. %s", excpt)
 
-    def set_id(self, id):
-        '''
+    def set_id(self, id_data):
+        '''Writes the module id to EEPROM
+        Args:
+            id_data (str): The ID of the Smart Module to write to EEPROM      
         Returns:
             None
         '''
-        if self.mock is False:
-            #Concatenate the 16 byte unique sensor address
-            for x in range(0,16):
-                if len(id) < x:
-                    ch = ""
-                else:
-                    ch = id[x]
 
-                self.ds3231.write_AT24C32_byte(x + 2, ord(ch))
+        try:
+            if not self.mock:
+                #Concatenate the 16 byte module ID
+                for x in range(0, LEN_ID):
+                    if len(id_data) < x + 1:
+                        ch = " "
+                    else:
+                        ch = id_data[x]
+
+                    self.ds3231.write_AT24C32_byte(x + LEN_TYPE, ord(ch))
+        except Exception, excpt:
+            self.logger.exception("Error writing Module ID to EEPROM. %s", excpt)
+
 
     def get_context(self):
-        '''
+        '''Gets the module context from the attached EEPROM
         Returns:
             str: 16-byte sensor context if mock is False. "Environment" if mock is True
         '''
 
-        if self.mock == True:
-            return "Environment"
-        else:
-            #Read the 16 byte asset context
-            context = ""
-            for x in range(18, 34):
-                context = context + str(self.ds3231.read_AT24C32_byte(x))
-            return id
+        try:
+            if self.mock:
+                return "Environment"
+            else:
+                #Read the 16 byte asset context
+                context = ""
+                for x in range(LEN_ID + LEN_TYPE,  LEN_ID + LEN_TYPE + LEN_CONTEXT):
+                    context = context + chr(self.ds3231.read_AT24C32_byte(x))
+                return str(context).strip()
+        except Exception, excpt:
+            self.logger.exception("Error reading Module context from EEPROM. %s", excpt)
 
     def set_context(self, context):
-        '''
+        '''Writes the modules sensor context to the attached EEPROM
+        Args:
+            context (str): The context of the Smart Modules sensor (Water, Light, etc)
         Returns:
             None
         '''
-        if self.mock is False:
-            #Write the 16 byte asset context
-            for x in range(0,16):
-                if len(context) < x:
-                    ch = ""
-                else:
-                    ch = id[x]
 
-                self.ds3231.write_AT24C32_byte(x + 18, ord(ch))
+        try:
+            if not self.mock:
+                #Write the 16 byte sensor context
+                for x in range(0,LEN_CONTEXT):
+                    if len(context) < x + 1:
+                        ch = " "
+                    else:
+                        ch = context[x]
 
+                    self.ds3231.write_AT24C32_byte(LEN_TYPE + LEN_ID + x, ord(ch))
+        except Exception, excpt:
+            self.logger.exception("Error writing Module context to EEPROM. %s", excpt)
