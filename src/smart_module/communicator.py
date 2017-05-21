9b836d6a7ab0d036c3a769ef55ddb815926581af
Pedro Freitas <pedrosffreitas@gmail.com>
2017-05-21 17:11:23 -0300

    Fixed call to loop_stop on communicator.

--- src/smart_module/communicator.py
+++ src/smart_module/communicator.py
@@ -51,7 +51,6 @@ class Communicator(object):
             self.client.connect(host=self.broker_ip, port=1883, keepalive=60)
         except Exception as excpt:
             self.logger.exception("[Exiting] Error connecting to broker: %s" % excpt)
-            self.client.loop_stop()
             sys.exit(-1)
 
     def subscribe(self, topic):
@@ -66,6 +65,7 @@ class Communicator(object):
         # We could implement a reconnect call.
         self.is_connected = False
         self.logger.info("[Exiting] Disconnected: %s" % mqtt.error_string(rc))
+        self.client.loop_stop()
         sys.exit(-1)
 
     # The callback for when the client receives a CONNACK response from the server.
